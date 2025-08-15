b4c0a2c2a5fa27a448296501226be2080e2700a2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-08-15 13:29:16 +0900

    drizzleの接続情報を一つにまとめた

--- src/db/connections.ts
+++ src/db/connections.ts
@@ -2,7 +2,9 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
+export const dbConnectionUrl = `postgresql://${process.env.PGSQL_USERNAME}:${process.env.PGSQL_ROOT_PASSWORD}@${process.env.PGSQL_HOST}:${process.env.PGSQL_PORT}/${process.env.PGSQL_DATABASE}`;
+
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.PGSQL_USERNAME}:${process.env.PGSQL_ROOT_PASSWORD}@${process.env.PGSQL_HOST}:${process.env.PGSQL_PORT}/${process.env.PGSQL_DATABASE}`,
+  connectionString: dbConnectionUrl,
 });
 export const db = drizzle({ client: pool });
